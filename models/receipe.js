11774f65dfda9a8f51b11419d7bf5129b88954df
Nael <nael@Air-de-Nael.com>
2020-05-20 23:14:40 +0200

    deployment

---
+++ models/receipe.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+
+
+const receipeSchema = new mongoose.Schema({
+    name: String,
+    image: String,
+    user: String
+});
+
+
+module.exports = mongoose.model("Receipe",receipeSchema);
